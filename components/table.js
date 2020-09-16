ba4617158667dc247efd3d7770579c5d94ea2e7b
Conner Vick <conner.vick@my.wheaton.edu>
2020-09-16 07:05:09 -0600

    refactor: merge tablehead into table

--- components/table.js
+++ components/table.js
@@ -1,13 +1,23 @@
-import TableHead from './tableHead'
+import styles from './table.module.css'
 
 function Table({ restaurants }){
-    function handleClick(e,val){
+    function changeSortMethod(e,val){
         console.log(val)
     }
 
     return( 
         <table className={"table table-dark"}>
-        <TableHead handleClick={handleClick}></TableHead>
+        <thead>
+            <tr>
+
+                <th className={styles.sortable} onClick={(e) => changeSortMethod(e,"name")}>Name</th>
+                <th className={styles.sortable} onClick={(e) => changeSortMethod(e,"city")}>City</th>
+                <th className={styles.sortable} onClick={(e) => changeSortMethod(e,"state")}>State</th>
+                <th className={styles.sortable} onClick={(e) => changeSortMethod(e,"phone")}>Phone Number</th>
+                <th className={styles.sortable} onClick={(e) => changeSortMethod(e,"genres")}>Genres</th>
+
+            </tr>
+        </thead>
         <tbody>
 
             {restaurants.map((restaurant) => (
